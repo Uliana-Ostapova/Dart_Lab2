1f0b90226021a8ef4e5af9993b5bdf602023f5a2
Uliana-Ostapova <ylanaostapova@gmail.com>
2026-09-14 10:53:37 +0300

    Add Todo class

--- bin/lab2_todo.dart
+++ bin/lab2_todo.dart
@@ -1,3 +1,13 @@
+import 'package:lab2_todo/todo.dart';
 void main() {
-  
+  Todo task = Todo(id: 1, title: 'Купить продукты');
+  print(task.id);
+  print(task.title);
+  print(task.isDone);
+
+  Todo task1 = Todo(id: 1, title: 'Купить продукты');
+  Todo task2 = Todo(id: 2, title: 'Сделать зарядку');
+  task1.complete();
+  print(task1);
+  print(task2);
 }
